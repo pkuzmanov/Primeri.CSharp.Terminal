0b49dfd8b06c934cf7200eaa3949b202e75a1efe
Plamen Kuzmanov <pkuzmanov2001@yahoo.com>
2016-04-13 19:22:59 +0300

    Създаване на програма Hello Aula

---
+++ HelloAula/HelloAula/HelloAula.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloAula
+{
+    class HelloAula
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aula!");
+        }
+    }
+}
