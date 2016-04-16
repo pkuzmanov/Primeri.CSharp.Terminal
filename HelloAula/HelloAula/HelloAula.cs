ee85e9bec19f11d780c8890a0dd0faf32616bc01
Plamen Kuzmanov <pkuzmanov2001@yahoo.com>
2016-04-16 13:15:49 +0300

    Модул-2, Видео-7 Работа с масиви

--- HelloAula/HelloAula/HelloAula.cs
+++ HelloAula/HelloAula/HelloAula.cs
@@ -6,7 +6,24 @@ namespace HelloAula
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Aula!");
+            string first1 = "Hello";
+            string second1 = "World";
+            object one = first1 + " " + second1;
+            string third = (string)one;
+            Console.WriteLine(third);
+
+            Console.WriteLine("Въведете едно число или цифра:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Въведете една дума: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(number.ToString().Length + word.Length);
+
+
+
+            /*Декларирайте две променливи от тип string и им присвоете стойности "Hello" и "World". Декларирайте променлива от тип object и й присвоете стойността на конкатенацията на двете променливи от тип string (не изпускайте интервала по средата). Декларирайте трета променлива от тип string и я инициализирайте със стойността на променливата от тип object. ( Hint: Трябва да използвате type casting.)
+             
+
+             * Да се въведат от командния ред 1 число или цифра и 1 дума/стринг. След това да изведе дължината на получения стринг при конкатенация, тоест броя на символите в новополучения стринг при конкатенация. (Hint: Трябва да преобразувате int към String и след това да конкатениратe.) */
         }
     }
 }
---
+++ Modul02/TableProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TableProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] row = new string[3];
+
+            row[0] = "Ред 1";
+            row[1] = "Ред 2";
+            row[2] = "Ред 3";
+
+            Console.WriteLine("Масив row:" + row[0] + ", " + row[1] + ", " + row[2] + "\n");
+
+            string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+            Console.WriteLine("Броят на цифрите в масива " + "1,2,3,4,5,6,7,8,9 e: " + parse.Length + "\n");
+
+            string list1 = string.Join(";", parse);
+            Console.WriteLine("Новият масив е: " + list1 + "\n");
+
+        }
+    }
+}
