81755e97bece78b3a36d3a8e2ea54a4fdfaeb729
Plamen Kuzmanov <pkuzmanov2001@yahoo.com>
2016-04-18 23:26:53 +0300

    Модул-3, Видео-3 Математически операции

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -10,13 +10,26 @@ namespace Modul03
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-            double a = 0, b = 0;
+            double a = 0, b = 0, c = 0;
             Console.Write("---> a = "); a = Convert.ToDouble(Console.ReadLine());
             Console.Write("---> b = "); b = Convert.ToDouble(Console.ReadLine());
            
-            string numberFormat = "C2";
+            string numberFormat = "#0.00 м3";
+
+            //Степенуване
+           
+            c = Math.Pow(a, b);
+            Console.WriteLine("\n---> a^b = " + (c).ToString(numberFormat));
             
-            Console.WriteLine("---> a/b = " + (a/b).ToString(numberFormat));
+            //Коренуване
+
+            c = Math.Sqrt(b);
+            Console.WriteLine("\n---> b^1/2 = " + (c).ToString(numberFormat));
+            
+            //Закръгления
+
+            c = Math.Round(a/b);
+            Console.WriteLine("\n---> a/b = " + (c).ToString());
         }
     }
 }
