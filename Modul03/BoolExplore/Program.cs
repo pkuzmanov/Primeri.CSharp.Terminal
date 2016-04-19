a93b0213deea08d7bde8874c4c1dadc1b09c9adf
Plamen Kuzmanov <pkuzmanov2001@yahoo.com>
2016-04-19 16:23:30 +0300

    Модул-3, Видео-5 Булеви операции

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -8,12 +8,23 @@ namespace BoolExplore
         static void Main(string[] args)
         {
             int[] a = new int[] { 4, 3, 2 };
+
             int userInterface = Convert.ToInt32(Console.ReadLine());
+
             bool check1 = userInterface <= a.Length;
             bool check2 = userInterface > 0;
-            Console.WriteLine(check1);
-            Console.WriteLine(check2);
-            Console.WriteLine(a[userInterface - 1]);
+
+            bool expression1 = check1 && check2; //Логическо И "&&"
+            bool expression2 = check1 || check2; //Логичско ИЛИ "||"
+            bool expression3 = !check1; //Отрицание "!"
+            
+            
+            Console.WriteLine("Логическо И: " + expression1.ToString());
+            Console.WriteLine("Логическо ИЛИ: " + expression2.ToString());
+            Console.WriteLine("Логическо Отрицание на " + check1.ToString() + ": " + expression3);
+                                          
+
+            //Console.WriteLine(a[userInterface - 1]);
         }
     }
 }
