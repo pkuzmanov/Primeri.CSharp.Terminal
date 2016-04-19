4bdf65a58be287c253eb40c4ebd0ac185c16b4db
Plamen Kuzmanov <pkuzmanov2001@yahoo.com>
2016-04-19 12:31:16 +0300

    Модул-3, Видео-4 Променливи от тип Bool

---
+++ Modul03/BoolExplore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace BoolExplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 4, 3, 2 };
+            int userInterface = Convert.ToInt32(Console.ReadLine());
+            bool check1 = userInterface <= a.Length;
+            bool check2 = userInterface > 0;
+            Console.WriteLine(check1);
+            Console.WriteLine(check2);
+            Console.WriteLine(a[userInterface - 1]);
+        }
+    }
+}
